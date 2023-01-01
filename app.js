0b38bf37c7b81e046ef63c44e1c34d9503baaec2
kidflash2jahaan <kidflash2jahaan@gmail.com>
2023-01-01 15:36:28 +0530

    Create Template and Find Day

---
+++ app.js
@@ -0,0 +1,42 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+
+const app = express()
+const port = 80
+
+app.set("view engine", "ejs")
+
+app.get("/", function (req, res) {
+    var date = new Date()
+    var day = date.getDay()
+
+
+    switch (day) {
+        case 0:
+          day = "Sunday";
+          break;
+        case 1:
+          day = "Monday";
+          break;
+        case 2:
+           day = "Tuesday";
+          break;
+        case 3:
+          day = "Wednesday";
+          break;
+        case 4:
+          day = "Thursday";
+          break;
+        case 5:
+          day = "Friday";
+          break;
+        case 6:
+          day = "Saturday";
+    }
+
+    res.render("list", { day: day })
+})
+
+app.listen(port, function () {
+    console.log("Server started on port " + port)
+})
