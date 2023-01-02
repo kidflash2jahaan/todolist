94ba2cf5b81a5c9168773a77347c41827db3a77a
kidflash2jahaan <kidflash2jahaan@gmail.com>
2023-01-02 11:48:29 +0530

    Create Functionality

--- app.js
+++ app.js
@@ -4,37 +4,30 @@ const bodyParser = require("body-parser")
 const app = express()
 const port = 80
 
+let items = []
+
 app.set("view engine", "ejs")
 
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("public"))
+
 app.get("/", function (req, res) {
-    var date = new Date()
-    var day = date.getDay()
-
-
-    switch (day) {
-        case 0:
-          day = "Sunday";
-          break;
-        case 1:
-          day = "Monday";
-          break;
-        case 2:
-           day = "Tuesday";
-          break;
-        case 3:
-          day = "Wednesday";
-          break;
-        case 4:
-          day = "Thursday";
-          break;
-        case 5:
-          day = "Friday";
-          break;
-        case 6:
-          day = "Saturday";
+    let date = new Date()
+    const options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
     }
+    let day = date.toLocaleDateString("en-US", options)
 
-    res.render("list", { day: day })
+    res.render("list", { day: day, items: items })
+})
+
+app.post("/", function (req, res) {
+    let item = req.body.newItem
+    items.push(item)
+
+    res.redirect("/")
 })
 
 app.listen(port, function () {
